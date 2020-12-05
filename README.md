eb9c28dfd5d4d1dfcbb11957356c303c20025e05
Tushar Madhukar <tushar.madhukar@gmail.com>
2020-12-05 14:38:31 +1100

    Keep Google and anonymous sign-in options only

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ A time tracker app based on Flutter and Firebase.
 ### Setup
 
 This app assumes a pre-configured Firebase project. To get one, follow the [Prerequisites](https://firebase.flutter.dev/docs/overview#prerequisites), [Installation](https://firebase.flutter.dev/docs/overview#installation) and platform-specific [setup](https://firebase.flutter.dev/docs/overview#platform-setup) steps.
+
+Note that following sign-in methods also need to be enabled in Firebase console (Authentication -> Sign-in method):
+1. Google
+2. Anonymous
+
+### Screenshots
+
+<img src="images/ios-signin.png" height="300em" />
--- lib/app/signin/signin_page.dart
+++ lib/app/signin/signin_page.dart
@@ -53,29 +53,6 @@ class SignInPage extends StatelessWidget {
             },
           ),
           SizedBox(height: 8.0),
-          SocialSignInButton(
-            text: 'Sign in with Facebook',
-            asset: 'images/facebook-logo.png',
-            color: Color(0xFF334D92),
-            textColor: Colors.white,
-            onPressed: () {},
-          ),
-          SizedBox(height: 8.0),
-          SignInButton(
-            text: 'Sign in with Email',
-            textColor: Colors.white,
-            color: Colors.teal[700],
-            onPressed: () {},
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            'or',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black87,
-            ),
-          ),
-          SizedBox(height: 8.0),
           SignInButton(
             text: 'Go Anonymous',
             textColor: Colors.black87,
